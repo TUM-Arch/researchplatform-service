ebe16852a154d0daf0c856efa6ec4214f4ba738c
Karthik Narumanchi <karthiknr@gmail.com>
2019-09-06 13:07:46 +0200

    Initial commit

---
+++ src/main/java/de/tum/ar/researchplatform/ResearchplatformApplication.java
@@ -0,0 +1,13 @@
+package de.tum.ar.researchplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tum/ar/researchplatform/ResearchplatformApplicationTests.java
@@ -0,0 +1,16 @@
+package de.tum.ar.researchplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResearchplatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
