1c7c8b9f17d3b9892b905094c67a830168c6d1b8
Karthik Narumanchi <karthiknr@gmail.com>
2019-09-11 00:31:46 +0200

    Add Constants, Remove keep files, Remove specified Mongo DB name

---
+++ src/main/java/de/tum/ar/researchplatform/util/Constants.java
@@ -0,0 +1,38 @@
+package de.tum.ar.researchplatform.util;
+
+/**
+ * Created by karthik on 9/10/2019
+ */
+
+/**
+ * Helper class to provide constants
+ */
+public final class Constants {
+
+    /**
+     * Enforce private constructor
+     */
+    private Constants() {}
+
+    /**
+     * Enums
+     */
+
+    //Enum for Field object
+    public enum FieldType {
+        DATE,
+        TIME,
+        RADIO,
+        SELECT,
+        CHECKBOX,
+        SWITCH,
+        TEXT,
+        MULTITEXT
+    }
+
+    //Enum for Project object
+    public enum ProjectStatus {
+        NOTSUBMITTED,
+        SUBMITTED
+    }
+}
