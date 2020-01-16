1036ac2911c5eb6efe33bf56559467af05355c40
Raymond Pinto <ge73lam@mytum.de>
2020-01-16 17:10:13 +0100

    User endpoints added to Controller

--- src/main/java/de/tum/ar/researchplatform/controller/UserController.java
+++ src/main/java/de/tum/ar/researchplatform/controller/UserController.java
@@ -1,9 +1,18 @@
 package de.tum.ar.researchplatform.controller;
 
+import de.tum.ar.researchplatform.model.User;
 import de.tum.ar.researchplatform.model.response.UsersResponseObject;
+
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,5 +28,45 @@ public interface UserController {
      */
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public UsersResponseObject getAllUsers();
-
+    
+    /**
+     * Endpoint to get a single User by id
+     * @return a single User
+     */
+    @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getUserById(@PathVariable String id);
+    
+    /**
+     * Endpoint to get a single User by name
+     * @return a single User
+     */
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getUserByName(@RequestParam String name);
+    
+    /**
+     * Endpoint to add a User
+     * @return a single User
+     */
+    @PostMapping(value = "/users")
+    public User addUser(@RequestBody User user);
+    
+    /**
+     * Endpoint to update a User
+     * @return a single User
+     */
+    @PutMapping(value = "/users")
+    public User updateUser(User user);
+    
+    /**
+     * Endpoint to delete a User
+     */
+    @DeleteMapping(value = "/users")
+    public void deleteAllUsers();
+    
+    /**
+     * Endpoint to delete a User by id
+     */
+    @DeleteMapping(value = "/users/{id}")
+    public void deleteUserById(@PathVariable String id);
+    
 }
--- src/main/java/de/tum/ar/researchplatform/controller/UserControllerImpl.java
+++ src/main/java/de/tum/ar/researchplatform/controller/UserControllerImpl.java
@@ -4,7 +4,9 @@ import de.tum.ar.researchplatform.model.User;
 import de.tum.ar.researchplatform.model.response.UsersResponseObject;
 import de.tum.ar.researchplatform.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by karthik on 9/9/2019
@@ -24,4 +26,36 @@ public class UserControllerImpl implements UserController {
         }
         return usersResponseObject;
     }
+    
+	@Override
+	public User getUserById(String id) {
+		User user = userService.findById(id);
+		return user;
+	}
+
+	@Override
+	public User getUserByName(String name) {
+		User user = userService.findByName(name);
+		return user;
+	}
+
+	@Override
+	public User addUser(User user) {
+		return userService.saveOrUpdate(user);
+	}
+
+	@Override
+	public User updateUser(User user) {
+		return userService.saveOrUpdate(user);
+	}
+
+	@Override
+	public void deleteAllUsers() {
+		userService.deleteAll();
+	}
+
+	@Override
+	public void deleteUserById(String id) {
+		userService.deleteById(id);
+	}
 }
