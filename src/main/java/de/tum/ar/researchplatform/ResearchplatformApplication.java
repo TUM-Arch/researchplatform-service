893501cba2c5a30d63422327573d954869215575
Karthik Narumanchi <karthiknr@gmail.com>
2020-04-18 20:56:00 +0200

    Update Security Config

--- src/main/java/de/tum/ar/researchplatform/ResearchplatformApplication.java
+++ src/main/java/de/tum/ar/researchplatform/ResearchplatformApplication.java
@@ -3,9 +3,11 @@ package de.tum.ar.researchplatform;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @Slf4j
 public class ResearchplatformApplication {
 
--- src/main/java/de/tum/ar/researchplatform/configuration/WebSecurityConfig.java
+++ src/main/java/de/tum/ar/researchplatform/configuration/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package de.tum.ar.researchplatform.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.ArrayList;
@@ -10,10 +12,15 @@ import java.util.ArrayList;
 /**
  * Created by karthik on 9/22/2019
  */
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // Force HTTPS
+        //http.requiresChannel().anyRequest().requiresSecure();
+
         ArrayList<String> allowedOriginsList = new ArrayList<>();
         allowedOriginsList.add("http://localhost:3000");
         CorsConfiguration corsConfiguration = new CorsConfiguration();
@@ -21,8 +28,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.applyPermitDefaultValues();
 
-        http.cors().configurationSource(request -> corsConfiguration);
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("*").permitAll();
+        http
+                .cors()
+                .configurationSource(request -> corsConfiguration);
+        http
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("*")
+                .permitAll();
     }
 }
